eea6ad60d286ba4e9cf09974494d9342a237653b
Leonardo Melo <leonardo.mel04@outlook.com>
2024-05-09 23:51:28 -0300

    Implementing simple login system with email and password

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,26 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { User } from 'src/entities/user.entity';
+import { LoginUserDTO } from 'src/user/dto/login-user.dto';
+import { UserService } from 'src/user/user.service';
+import * as bcrypt from 'bcryptjs';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(private userService: UserService) {}
+
+  async login(loginDTO: LoginUserDTO): Promise<User> {
+    const user = await this.userService.findOne(loginDTO);
+
+    const passwordMatched = await bcrypt.compare(
+      loginDTO.password,
+      user.password,
+    );
+
+    if (passwordMatched) {
+      delete user.password;
+      return user;
+    } else {
+      throw new UnauthorizedException('Password does not match');
+    }
+  }
+}
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from 'src/entities/user.entity';
 import { Repository } from 'typeorm';
@@ -16,6 +16,14 @@ export class UserService {
     userDTO.password = await bcrypt.hash(userDTO.password, salt);
     const user = await this.userRepository.save(userDTO);
     delete user.password;
+    return user;
+  }
+
+  async findOne(data: Partial<User>): Promise<User> {
+    const user = await this.userRepository.findOneBy({ email: data.email });
+    if (!user) {
+      throw new UnauthorizedException('Could not find the user');
+    }
     return user;
   }
 }
